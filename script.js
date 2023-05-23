9ed1c428ec0c0c3120bd9d973dd5ccd8d7a5946f
SirFrank0 <frankseb04@gmail.com>
2023-05-23 17:54:08 -0500

    edit JS code for contrast

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 
+
+
+
 document.getElementById('generateButton').addEventListener('click', generarColoresAleatorios);
 
 function generarColoresAleatorios() {
   var colorAleatorio1 = generarColorAleatorio();
-  var colorAleatorio2 = generarColorAleatorio();
+  var colorAleatorio2 = generarColorContraste(colorAleatorio1);
   var colorAleatorio3 = generarColorAleatorio();
 
   document.documentElement.style.setProperty('--color60', colorAleatorio1);
   document.documentElement.style.setProperty('--color30', colorAleatorio2);
   document.documentElement.style.setProperty('--color10', colorAleatorio3);
 
-
-
-
   var colorValuesDiv = document.getElementById('colorValues');
   colorValuesDiv.innerHTML = `
-  Color 60%: ${colorAleatorio1}<br> <div style="background-color: ${colorAleatorio1}; width: 50px; height: 25px;"></div> 
-  Color 30%: ${colorAleatorio2}<br> <div style="background-color: ${colorAleatorio2}; width: 50px; height: 25px;"></div> 
-  Color 10%: ${colorAleatorio3}<div style="background-color: ${colorAleatorio3}; width: 50px; height: 25px;"></div> 
+    Color 60%: ${colorAleatorio1}<br> <div style="background-color: ${colorAleatorio1}; width: 50px; height: 25px;"></div> 
+    Color 30%: ${colorAleatorio2}<br> <div style="background-color: ${colorAleatorio2}; width: 50px; height: 25px;"></div> 
+    Color 10%: ${colorAleatorio3}<div style="background-color: ${colorAleatorio3}; width: 50px; height: 25px;"></div> 
   `;
 }
 
@@ -32,6 +32,22 @@ function generarColorAleatorio() {
   return color;
 }
 
+function generarColorContraste(color) {
+  var luminosidad = calcularLuminosidad(color);
+  var colorContraste = luminosidad > 0.5 ? '#000000' : '#ffffff';
+  return colorContraste;
+}
+
+function calcularLuminosidad(color) {
+  var r = parseInt(color.substring(1, 3), 16) / 255;
+  var g = parseInt(color.substring(3, 5), 16) / 255;
+  var b = parseInt(color.substring(5, 7), 16) / 255;
+
+  var max = Math.max(r, g, b);
+  var min = Math.min(r, g, b);
+
+  return (max + min) / 2;
+}
 
 
 
@@ -54,22 +70,7 @@ function generarColorAleatorio() {
 
 
 
-//  var colorAleatorio = generarColorAleatorio();
 
 
-//  document.getElementById('generateButton').addEventListener('click', generarColorAleatorio);
 
-//  document.documentElement.style.setProperty('--navBarItems', colorAleatorio);
-//  document.documentElement.style.setProperty('--bgSearchbar', colorAleatorio);
 
-//  function generarColorAleatorio() {
-//      var caracteres = '0123456789ABCDEF';
-//      var color = '#';
-  
-//      for (var i = 0; i < 6; i++) {
-//        color += caracteres[Math.floor(Math.random() * 16)];
-//      }
-  
-//      document.documentElement.style.setProperty('--navBarItems', color);
-//      document.documentElement.style.setProperty('--bgSearchbar', color);
-//    }
