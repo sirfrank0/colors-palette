cc5f30d057dca464c191e97675ab941323f89be6
SirFrank0 <frankseb04@gmail.com>
2023-05-22 22:23:37 -0500

    complete function

--- script.js
+++ script.js
@@ -1,18 +1,66 @@
-var elemento = document.getElementById('center-blockJS');
-var colorAleatorio = generarColorAleatorio();
 
+document.getElementById('generateButton').addEventListener('click', generarColoresAleatorios);
 
-document.getElementById('generateButton').addEventListener('click', generarColorAleatorio);
+function generarColoresAleatorios() {
+  var colorAleatorio1 = generarColorAleatorio();
+  var colorAleatorio2 = generarColorAleatorio();
+  var colorAleatorio3 = generarColorAleatorio();
 
-document.documentElement.style.setProperty('--navBarItems', colorAleatorio);
+  document.documentElement.style.setProperty('--color60', colorAleatorio1);
+  document.documentElement.style.setProperty('--color30', colorAleatorio2);
+  document.documentElement.style.setProperty('--color10', colorAleatorio3);
+}
 
 function generarColorAleatorio() {
-    var caracteres = '0123456789ABCDEF';
-    var color = '#';
-  
-    for (var i = 0; i < 6; i++) {
-      color += caracteres[Math.floor(Math.random() * 16)];
-    }
-  
-    document.documentElement.style.setProperty('--navBarItems', color);
+  var caracteres = '0123456789ABCDEF';
+  var color = '#';
+
+  for (var i = 0; i < 6; i++) {
+    color += caracteres[Math.floor(Math.random() * 16)];
   }
+
+  return color;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  var colorAleatorio = generarColorAleatorio();
+
+
+//  document.getElementById('generateButton').addEventListener('click', generarColorAleatorio);
+
+//  document.documentElement.style.setProperty('--navBarItems', colorAleatorio);
+//  document.documentElement.style.setProperty('--bgSearchbar', colorAleatorio);
+
+//  function generarColorAleatorio() {
+//      var caracteres = '0123456789ABCDEF';
+//      var color = '#';
+  
+//      for (var i = 0; i < 6; i++) {
+//        color += caracteres[Math.floor(Math.random() * 16)];
+//      }
+  
+//      document.documentElement.style.setProperty('--navBarItems', color);
+//      document.documentElement.style.setProperty('--bgSearchbar', color);
+//    }
